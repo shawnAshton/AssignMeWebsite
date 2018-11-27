f58160faff9654afda7303f19496fcdbc7fbb892
shawnAshton <ash14009@byui.edu>
2018-11-26 23:13:07 -0700

    a

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ function getProject(req,res)
 
 function getProjectFromDB(id, callback)
 {
-   var sql = "SELECT title * project WHERE id = $1::int";
+   var sql = "SELECT title, program_user_id project WHERE id = $1::int";
    var params = [id];
    pool.query(sql,params,function(err,result)
    {
