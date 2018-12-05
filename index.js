3ed2c6c9bfa5708dc744452791f77136e00f0cba
shawnAshton <ash14009@byui.edu>
2018-12-04 17:55:22 -0700

    a

--- index.js
+++ index.js
@@ -141,7 +141,7 @@ function authenticate(req,res)
       }
       else
       {
-         res.status(200).json(result);
+         // res.status(200).json(result);
          console.log("ok...im almost there...\nindex 0 is this!!!!!!!!!!!!!!!!!!!!");
          // I NEED TO FIGURE OUT HOW TO USE RESULTS
          var params = {};
