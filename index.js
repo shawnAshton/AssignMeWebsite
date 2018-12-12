51bc08f78a6625572dc862c3302020c8ee05ca7d
shawnAshton <ash14009@byui.edu>
2018-12-12 16:24:10 -0700

    a

--- index.js
+++ index.js
@@ -260,31 +260,34 @@ function createUser(req,res)
          exists = false;
          console.log("       POOOOOOOOOOOOOOOOTAYTO         ")
       }
-      
+      console.log("exists is AY", exists);
+      if (!exists)
+      {
+         var sql = "INSERT INTO program_user(username,password) VALUES ($1, $2)";
+         var params = [newUsername,newPassword];
+         pool.query(sql,params,function(err)
+         {
+            if (err)
+            {
+               console.log("error in createUser");
+               res.status(500).json({success: false, data: error});
+            }
+            else
+            {
+               res.status(200).json({success: true, data: "success in creation"});
+            }
+         })
+      }
+      else
+      {
+         res.status(500).json({success: false, data: "it already exists"});
+      }
+
+
    })
    console.log("exists is", exists);
    // create it if it doesnt exist
-   if (!exists)
-   {
-      var sql = "INSERT INTO program_user(username,password) VALUES ($1, $2)";
-      var params = [newUsername,newPassword];
-      pool.query(sql,params,function(err)
-      {
-         if (err)
-         {
-            console.log("error in createUser");
-            res.status(500).json({success: false, data: error});
-         }
-         else
-         {
-            res.status(200).json({success: true, data: "success in creation"});
-         }
-      })
-   }
-   else
-   {
-      res.status(500).json({success: false, data: "it already exists"});
-   }
+   
 
 }
 
