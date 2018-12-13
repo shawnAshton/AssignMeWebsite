aca7697a9fc914823c62d60bdae0ced4c610706a
shawnAshton <ash14009@byui.edu>
2018-12-13 12:25:06 -0700

    a

--- index.js
+++ index.js
@@ -314,7 +314,7 @@ function createProject(req,res)
             console.log("error in query: ")
             console.log(err);
          }
-         console.log("Found result in create user.. the username is... " + result.rows.id);
+         console.log("Found result in create user.. the username is... " + JSON.stringify(result.rows));
       })
 
 
